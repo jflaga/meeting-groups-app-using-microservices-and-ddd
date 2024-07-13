8751219d638dbd070a2b212d8db2ee4f807c4da6
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2024-07-14 05:07:35 +0800

    feat: add sample call to external API with authorization policy

--- src/MyMeetings/MyMeetings.Keycloak.TestWebApi/Program.cs
+++ src/MyMeetings/MyMeetings.Keycloak.TestWebApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.Runtime;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,12 +26,30 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         {
             //ValidateIssuer = authServerSettings.ValidateIssuer, // set to true be default??
         };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnTokenValidated = context =>
+            {
+                //context.Principal.AddIdentity(new ClaimsIdentity(
+                //    new List<Claim>
+                //    {
+                //    new Claim(ClaimTypes.Name, "admin")
+                //    }));
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorizationBuilder()
-    .SetDefaultPolicy(new AuthorizationPolicyBuilder()    
+    .SetDefaultPolicy(new AuthorizationPolicyBuilder()
         .RequireAuthenticatedUser()
         //.RequireClaim("email_verified", "true")
+        .Build())
+    .AddPolicy("Administrator", new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        //.RequireUserName("admin")
+        .RequireClaim("name", "admin")
         .Build());
 
 var app = builder.Build();
@@ -66,6 +85,22 @@ app.MapGet("/weatherforecast", () =>
 })
 .RequireAuthorization()
 .WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/weatherforecast_admin", () =>
+{
+    var forecast = Enumerable.Range(1, 15).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.RequireAuthorization(policyNames: "Administrator")
+.WithName("GetWeatherForecast_Admin")
 .WithOpenApi();
 
 app.Run();
